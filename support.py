a4a6ecfff77576409106d04bc62fd03181b507a2
ronrest <ronny.coding@gmail.com>
2017-07-04 15:48:09 +1000

    DOCS: Add docstring to random_substring()

--- support.py
+++ support.py
@@ -1,5 +1,11 @@
 import random
+# ==============================================================================
+#                                                               RANDOM_SUBSTRING
+# ==============================================================================
 def random_substring(s, size=200):
+    """ Given a string and optionally a sample size (which defaults to 200),
+        it returns a random substring from s of length `size`.
+    """
     i = random.randint(0, len(s) - size)
     return s[i:i + size]
 
