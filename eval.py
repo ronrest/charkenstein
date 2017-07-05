36651b8e2c8997d4a2117ccc3b5a8b07e7f37a66
ronrest <ronny.coding@gmail.com>
2017-07-05 21:41:17 +1000

    FEAT: Add eval_sequence()

--- eval.py
+++ eval.py
@@ -32,3 +32,29 @@ def create_eval_batch(data, char2id, start_i, seq_length=200, batch_size=1):
     
     return X, Y
 
+def eval_sequence(model, X, Y):
+    # Get dimensions of input and target labels
+    msg = "X and Y should be shape [n_batch, sequence_length]"
+    assert len(X.size()) == len(Y.size()) == 2, msg
+    batch_size, sample_length = X.size()
+
+    # Store the original training mode, and turn training mode Off
+    train_mode = model.training
+    model.train(False)
+    
+    # Initialize hidden state and reset the accumulated gradients
+    hidden = model.init_hidden(batch_size)
+    model.zero_grad() # TODO: This might be irrelevant for evaluation
+    
+    # Loop through each item in the sequence
+    loss = 0
+    for i in range(sample_length):
+        output, hidden = model(X[:, i], hidden)
+        loss += model.loss_func(output, Y[:, i])
+    
+    # Restore the original training mode
+    model.train(train_mode)
+
+    # Return the average loss over the batch of sequences
+    return loss.data[0] / sample_length
+
